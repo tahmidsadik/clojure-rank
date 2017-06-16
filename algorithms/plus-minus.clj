9de67e01a228d749a93e4d27735bb09727b4b11e
Tahmid Sadik <tahmidsadik112@gmail.com>
2017-06-17 00:00:00 +0600

    Adds solution to plus-minus from algorithms

---
+++ algorithms/plus-minus.clj
@@ -0,0 +1,24 @@
+(require '[clojure.string :as str])
+
+(defn take-input-line []
+  (->>
+   (->
+    (read-line)
+    (str/split #"\s+"))
+   (map #(Integer/parseInt %))))
+
+(def array-size (Integer/parseInt (read-line)))
+(def input-array (take-input-line))
+
+(def positive-nums (filter #(> % 0) input-array))
+(def negative-nums (filter #(< % 0) input-array))
+(def zeroes (filter #(= % 0) input-array))
+
+(print
+ (format "%.6f\n%.6f\n%.6f"
+         (double (/ (count positive-nums) array-size))
+         (double (/ (count negative-nums) array-size))
+         (double (/ (count zeroes) array-size))))
+
+
+
